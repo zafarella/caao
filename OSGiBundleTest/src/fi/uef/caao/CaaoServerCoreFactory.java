f59462471cb6f2cd2bc998516894c3a981e58cd1
Zafar Haydarov <zafarella@gmail.com>
2012-04-21 12:28:24 +0200

    Import from CVS

---
+++ OSGiBundleTest/src/fi/uef/caao/CaaoServerCoreFactory.java
@@ -0,0 +1,43 @@
+package fi.uef.caao;
+
+
+
+/**
+ * The class <code>CaaoServerCoreFactory</code> implements static methods that return instances of the class <code>{@link CaaoServerCore}</code>.
+ *
+ * @generatedBy CodePro at 4/28/11 3:38 AM
+ * @author zafar.khaydarov
+ * @version $Revision: 1.3 $
+ */
+public class CaaoServerCoreFactory
+ {
+	/**
+	 * Prevent creation of instances of this class.
+	 *
+	 * @generatedBy CodePro at 4/28/11 3:38 AM
+	 */
+	private CaaoServerCoreFactory() {
+	}
+
+
+	/**
+	 * Create an instance of the class <code>{@link CaaoServerCore}</code>.
+	 *
+	 * @generatedBy CodePro at 4/28/11 3:38 AM
+	 * @return CaaoServerCore
+	 */
+	public static CaaoServerCore createCaaoServerCore() {
+		return new CaaoServerCore();
+	}
+
+
+	/**
+	 * Create an instance of the class <code>{@link CaaoServerCore}</code>.
+	 *
+	 * @generatedBy CodePro at 5/10/11 1:01 PM
+	 * @return CaaoServerCore
+	 */
+	public static CaaoServerCore createCaaoServerCore2() {
+		return new CaaoServerCore();
+	}
+}
