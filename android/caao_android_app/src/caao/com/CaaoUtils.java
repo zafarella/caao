455f8054871c01e1c20c994e3dad0458d7f8a9f4
zafarella Linux <zafarella@gmail.com>
2013-02-12 01:29:32 +0100

    -- removed lib folder with apache dependencies

---
+++ android/caao_android_app/src/caao/com/CaaoUtils.java
@@ -0,0 +1,30 @@
+/**
+ * University of Eastern Finland
+ * Computer science department
+ * Project: Context Aware Orginizer
+ * Author: Zafar Khaydarov
+ * E-mail: zkhayda@uef.fi
+ * Web: cs.joensuu.fi/~zkhayda
+ * Date: Apr 26, 2012
+ */
+package caao.com;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * @author z1
+ */
+public class CaaoUtils extends Activity {
+
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ android/caao_android_app/src/caao/com/settings_activities/Advanced_settings.java
@@ -0,0 +1,35 @@
+/**
+ *  Computer science department 
+ * Project: Context Aware Orginizer 
+ * Author: Zafar Khaydarov
+ * E-mail: zkhayda@uef.fi 
+ * Web: cs.joensuu.fi/~zkhayda 
+ * Date: Mar 24, 2011
+ */
+package caao.com.settings_activities;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import caao.com.R;
+
+/**
+ * Advanced preferences - the submenu in settings. Displays bunch of advanced
+ * preferences.
+ *
+ * @author zafar.khaydarov
+ * @version $Revision: 1.8 $
+ */
+public class Advanced_settings extends PreferenceActivity {
+    /**
+     * @param Saved_Instance_State Bundle
+     */
+    @Override
+    public void onCreate(Bundle Saved_Instance_State) {
+        super.onCreate(Saved_Instance_State);
+        // natifying the Preference manager to save the settings after user have
+        // changed them
+        getPreferenceManager().setSharedPreferencesName("advanced_preferences");
+        // displaying the preferences
+        addPreferencesFromResource(R.xml.advanced_preferences);
+    }
+}
