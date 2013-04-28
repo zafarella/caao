3325aa868f144d656e24200445717327d98eb020
zafarella-Linux <zafarella@gmail.com>
2013-04-28 19:25:02 +0200

    -- correcting typos in mysql db creation script

--- databases/caao_mysql/create_db_UNIX.sh
+++ databases/caao_mysql/create_db_UNIX.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-mysql -u root -p root -h localhost < CAAO_DB_MYSQL_MOD_CORE_MOD_PG_unix_for_new_db.sql
-mysql -u root -p root -h localhost < data_unix.sql
+mysql --user=root --password=root -h localhost < CAAO_DB_MYSQL_MOD_CORE_MOD_PG_unix_for_new_db.sql
+mysql --user=root --password=root -h localhost < data_unix.sql
