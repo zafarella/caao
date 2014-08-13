049a1a7c84f40b600a60874ae4a910cc7078cfd7
zafarella-Linux <zafarella@gmail.com>
2014-08-13 02:07:44 -0400

    -- remove of redundant groupId from child poms

--- databases/mysql/create_db_UNIX.sh
+++ databases/mysql/create_db_UNIX.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/sh
 
 mysql --user=root --password=root -h localhost < sql/CAAO_DB_MYSQL_MOD_CORE_MOD_PG_unix_for_new_db.sql
 mysql --user=root --password=root -h localhost < sql/data_unix.sql
