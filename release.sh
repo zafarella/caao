5144c1fad92962669bd39c0d651285a12149e688
zafarella-Linux <zafarella@gmail.com>
2014-09-11 20:31:29 -0400

    -- poms versions updates due to failed release process

--- release.sh
+++ release.sh
@@ -12,19 +12,9 @@ git status;
 #mvn versions:display-plugin-updates
 
 
-read -p "Please make sure all is good before starting release. \n Press [Y] when ready, or any other key to exit the process.";
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    mvn -Dmaven.artifact.threads=20 release:prepare release:perform -T4C ;
-fi
+    mvn -Dmaven.artifact.threads=20 release:prepare release:perform -T4C -B ;
 
-
-echo "Would you like to make git cleanup on your local repo? It will keep your git speedy.(Y/n)\n";
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
     git gc
-fi
-
 
 # to sync the tags in remote and local repos:
 
